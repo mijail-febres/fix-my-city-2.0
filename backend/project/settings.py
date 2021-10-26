5448b224d03113dcfb091f74761866bf64429edd
Adrien <adrien.laaboudi@gmail.com>
2021-10-26 14:25:40 +0200

    CORS fix (test)

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -67,15 +67,17 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'project.urls'
 
-CORS_ALLOWED_ORIGINS = [
-        "https://fix-my-city.propulsion-learn.ch",
-        "http://localhost:8000",
-        "http://localhost:3000",
-        "http://127.0.0.1:8000",
-        "http://127.0.0.1:3000",
-        "http://192.168.178.30:3000",
-        "http://192.168.1.9:3000"
-]
+# CORS_ALLOWED_ORIGINS = [
+#         "https://fix-my-city.app.propulsion-learn.ch",
+#         "http://localhost:8000",
+#         "http://localhost:3000",
+#         "http://127.0.0.1:8000",
+#         "http://127.0.0.1:3000",
+#         "http://192.168.178.30:3000",
+#         "http://192.168.1.9:3000"
+# ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
