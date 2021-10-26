c2db6d80ef1074e741a1ae1ee06212a0a4342613
Adrien <adrien.laaboudi@gmail.com>
2021-10-26 13:47:42 +0200

    Fixed registration process

--- backend/registration/models.py
+++ backend/registration/models.py
@@ -1,7 +1,8 @@
 import random
 
 from django.contrib.auth import get_user_model
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from django.db import models
 
 User = get_user_model()
@@ -13,7 +14,24 @@ def code_generator(length=6):
 
 
 class Registration(models.Model):
-    code = models.CharField(max_length=6, default=code_generator)
-    subject = models.CharField(max_length=1, choices=[("R", "registration"), ("P", "password_reset")], default="R")
-    is_used = models.BooleanField(default=False)
-    user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name='registration', primary_key=True)
+    status_choices = [
+        (1, 'Inactive'),
+        (2, 'Activated')
+    ]
+
+    code = models.IntegerField(default=code_generator)
+    email = models.EmailField(max_length=400, blank=True, null=True, unique=True)
+    status = models.IntegerField(choices=status_choices, default=1)
+
+    def __str__(self):
+        return f'ID {self.id}: Registration profile from {self.email}'
+
+    @receiver(post_save, sender=User)
+    def create_registration_profile(sender, instance, **kwargs):
+        profile, created = Registration.objects.get_or_create(email=instance.email)
+        if created:
+            profile.status = 2
+            profile.save()
+        else:
+            profile.status = 2
+            profile.save()
--- backend/registration/serializers.py
+++ backend/registration/serializers.py
@@ -1,21 +1,46 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from .models import Registration
 
 User = get_user_model()
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(min_length=1)
+
+    def validate(self, data):
+        valid_email = data.get('email', 0)
+        if Registration.objects.filter(email__exact=valid_email):
+            raise serializers.ValidationError('Email already taken')
+        return data
+
     class Meta:
-        model = User
+        model = Registration
         fields = ['email']
 
 
 class ValidationSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(min_length=1)
+    username = serializers.CharField(max_length=75, min_length=1)
+    code = serializers.CharField(min_length=1)
+    password = serializers.CharField(min_length=1)
+    password_repeat = serializers.CharField(min_length=1)
+
+    def validate(self, data):
+        request_email = data.get('email', 0)
+        request_code = data.get('code', 0)
+        request_password = data.get('password', 0)
+        request_password_repeat = data.get('password_repeat', 0)
+        if not Registration.objects.filter(email__exact=request_email):
+            raise serializers.ValidationError('Wrong email')
+        if not Registration.objects.filter(code__exact=request_code):
+            raise serializers.ValidationError('Wrong code')
+        if not Registration.objects.filter(email__exact=request_email, code__exact=request_code):
+            raise serializers.ValidationError('Email and registration code do not match')
+        if request_password != request_password_repeat:
+            raise serializers.ValidationError('The two passwords do not match')
+        return data
+
     class Meta:
-        model = User
-        fields = [
-            'email',
-            'username',
-            'first_name',
-            'last_name',
-        ]
+        model = Registration
+        fields = ['id', 'email', 'code', 'username', 'password', 'password_repeat']
