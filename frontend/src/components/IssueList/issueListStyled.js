af8abb540d6cbe6eceb73b5c489da0540eb08102
pearlimbach <pearlpillaert@gmail.com>
2021-11-01 11:18:12 +0100

    Menufooter for issuelist

---
+++ frontend/src/components/IssueList/issueListStyled.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const ListWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  margin-top: 12px;
+  border-radius: 3px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Main = styled.div`
+  width: 90%;
+
+  margin-left: 2%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Title = styled.div`
+  width: 100%;
+  font-size: 25px;
+  font-weight: bold;
+`;
