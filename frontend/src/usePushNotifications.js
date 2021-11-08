4eb797bab16621259e9cc37710b270b7c0c45deb
pearlimbach <pearlpillaert@gmail.com>
2021-11-08 08:59:48 +0100

    push notification test

---
+++ frontend/src/usePushNotifications.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+
+import {
+    isPushNotificationSupported,
+    askUserPermission,
+    registerServiceWorker,
+} from "../src/pages/Homepage/Homepage";
+
+const pushNotificationSupported = isPushNotificationSupported();
+//check if the push notifications are supported by the browser
+
+export default function usePushNotifications() {
+  const [userConsent, setSuserConsent] = useState(Notification.permission);
+  //to manage the user consent: Notification.permission is a JavaScript native function that return the current state of the permission
+  const [error, setError] = useState(null);
+  //to manage errors
+  const [loading, setLoading] = useState(true);
+  //to manage async actions
+
+  useEffect(() => {
+    if (pushNotificationSupported) {
+      setLoading(true);
+      setError(false);
+      registerServiceWorker().then(() => {
+        setLoading(false);
+      });
+    }
+  }, []);
+  //if the push notifications are supported, registers the service worker
+  //this effect runs only the first render
+  
+
+  /**
+   * define a click handler that asks the user permission,
+   * it uses the setSuserConsent state, to set the consent of the user
+   * If the user denies the consent, an error is created with the setError hook
+   */
+  const onClickAskUserPermission = () => {
+    setLoading(true);
+    setError(false);
+    askUserPermission().then(consent => {
+      setSuserConsent(consent);
+      if (consent !== "granted") {
+        setError({
+          name: "Consent denied",
+          message: "You denied the consent to receive notifications",
+          code: 0
+        });
+      }
+      setLoading(false);
+    });
+  };
+ 
+  return {
+    onClickAskUserPermission, 
+    userConsent,
+    pushNotificationSupported,
+    error,
+    loading
+  };
+}
